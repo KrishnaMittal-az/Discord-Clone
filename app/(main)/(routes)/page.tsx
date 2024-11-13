7df947f510b815aebdadf9f97e1b72de4f9ddf34
krishnamittal-az <krishnamittal30062004@gmail.com>
2024-11-13 15:09:52 +0530

    new files added

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,14 @@
+// import Image from "next/image";
+// import {cn} from "@/lib/utils";
+// import { Button } from "@/components/ui/button";
+// import { Ghost } from "lucide-react";
+
+// const state = true;
+
+export default function Home() {
+  return (
+   <div>
+    This is a protected Route.
+   </div>
+  );
+}
